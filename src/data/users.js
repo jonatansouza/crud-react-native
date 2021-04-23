def27764ddf7e435344ade9a27c5bed62df4f3ce
jonatan gall <jonatangd.souza@gmail.com>
2021-04-23 01:50:23 -0300

    update users

--- src/data/users.js
+++ src/data/users.js
@@ -3,12 +3,12 @@ export default [
         id: 1,
         name: 'Tux Casual',
         email: 'tc@gmail.com',
-        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295431__480.png'
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430__480.png'
     },
     {
         id: 2,
         name: 'Tux doidão',
         email: 'td@gmail.com',
-        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430__480.png'
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295431__480.png'
     }
 ];
